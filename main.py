17abe731de96d5a081de1d5ed659cb3281a07ed1
Zia <goldenman668@outlook.com>
2021-01-03 23:23:20 +0800

    fix: add some log

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if root_path[-1] == "/":
     root_path = root_path[:-1]
 jpg_path = root_path + "/jpg"
 raw_path = root_path + "/raw"
-selected_path = root_path + "/selected"
+selected_path = root_path + "/raw_selected"
 raw_suffix = ".ARW"
 
 
@@ -43,13 +43,20 @@ if not ret:
     exit(-1)
 jpg_file_lists = os.listdir(jpg_path)
 raw_file_lists = os.listdir(raw_path)
-print(jpg_file_lists)
+process_count = 1
+fail_count = 0
 for jpg_file_whole_name in jpg_file_lists:
     jpg_file_name = jpg_file_whole_name.split(".")[0]
+    length = len(jpg_file_lists)
     try:
+        print("正在处理 " + jpg_file_name + "， 进度：{:.2%}".format(process_count / length))
         index = raw_file_lists.index(jpg_file_name + raw_suffix)
         if index >= 0:
             shutil.copy(raw_path + "/" + raw_file_lists[index], selected_path)
             # print(raw_path+"/"+raw_file_lists[index])
     except ValueError as err:
         print("Handling run-time error:", err)
+        fail_count += 1
+    finally:
+        process_count += 1
+print("共计" + str(length) + "个，失败" + str(fail_count) + "个")
