5accc430589682c2dac398b9cf89460287df5f45
Zia <goldenman668@outlook.com>
2021-01-03 21:07:44 +0800

    feature(add new feature): realize the base feature

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :main.py
+# @Time      :2021/1/3 3:03 下午
+# @Author    :goldenman
+import os
+import shutil
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+if __name__ == "__main__":
+    run_code = 0
+
+root_path = "/Users/goldenman/Desktop/12月广东"
+# root_path = input("Please enter the root path (option + command + C):")
+if root_path[-1] == "/":
+    root_path = root_path[:-1]
+jpg_path = root_path + "/jpg"
+raw_path = root_path + "/raw"
+selected_path = root_path + "/selected"
+raw_suffix = ".ARW"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def check_path():
+    path_ret = os.access(root_path, os.F_OK)
+    if not path_ret:
+        print("Wrong root path")
+        return path_ret
+    path_ret = os.access(jpg_path, os.F_OK)
+    if not path_ret:
+        print("Wrong jpg path")
+        return path_ret
+    path_ret = os.access(raw_path, os.F_OK)
+    if not path_ret:
+        print("Wrong raw path")
+        return path_ret
+    path_ret = os.access(selected_path, os.F_OK)
+    if not path_ret:
+        os.makedirs(selected_path)
+    return path_ret
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ret = check_path()
+if not ret:
+    exit(-1)
+jpg_file_lists = os.listdir(jpg_path)
+raw_file_lists = os.listdir(raw_path)
+print(jpg_file_lists)
+for jpg_file_whole_name in jpg_file_lists:
+    jpg_file_name = jpg_file_whole_name.split(".")[0]
+    try:
+        index = raw_file_lists.index(jpg_file_name + raw_suffix)
+        if index >= 0:
+            shutil.copy(raw_path + "/" + raw_file_lists[index], selected_path)
+            # print(raw_path+"/"+raw_file_lists[index])
+    except ValueError as err:
+        print("Handling run-time error:", err)
